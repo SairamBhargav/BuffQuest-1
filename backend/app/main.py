8e0f429060b77316800da22c65f2226fa6cf2f11
Blake Huhn <63067531+ItsAltus@users.noreply.github.com>
2026-03-08 08:40:35 -0600

    Add Vercel app to CORS allowed origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -29,6 +29,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://0.0.0.0:3000",
+        "https://buffquest.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
